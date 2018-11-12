ec09b0ab6fc561ecb0d68efd77e69022f2edd0b5
Ivan Kušt <ivan.kust@toptal.com>
2018-11-12 22:25:21 +0100

    Implemented using /v2/getcodeactions and /v2/runcodeaction.

--- commands/code_actions.py
+++ commands/code_actions.py
@@ -7,10 +7,16 @@ from ..lib import helpers
 
 class OmniSharpCodeActions(sublime_plugin.TextCommand):
     data = None
-    selectionStartColumn = 0
-    selectionStartLine = 0
-    selectionEndColumn = 0
-    selectionEndLine = 0
+    selection = {
+        "Start": {
+            "Line": 0,
+            "Column": 0
+        },
+        "End": {
+            "Line": 0,
+            "Column": 0
+        }
+    }
 
     def run(self, edit):
         if self.data is None:
@@ -24,20 +30,17 @@ class OmniSharpCodeActions(sublime_plugin.TextCommand):
                 location = selection[0]
                 cursor = self.view.rowcol(location.begin())
                 
-                self.selectionStartLine = cursor[0] + 1
-                self.selectionStartColumn = cursor[1] + 1
+                self.selection["Start"]["Line"] = cursor[0] + 1
+                self.selection["Start"]["Column"] = cursor[1] + 1
 
                 othercursor = self.view.rowcol(location.end())
-                self.selectionEndLine = othercursor[0] + 1
-                self.selectionEndColumn = othercursor[1] + 1
+                self.selection["End"]["Line"] = othercursor[0] + 1
+                self.selection["End"]["Column"] = othercursor[1] + 1
 
-                params['selectionStartColumn'] = self.selectionStartColumn
-                params['selectionStartLine'] = self.selectionStartLine
-                params['selectionEndColumn'] = self.selectionEndColumn
-                params['selectionEndLine'] = self.selectionEndLine
+                params['Selection'] = self.selection
 
             omnisharp.get_response(
-                self.view, '/getcodeactions', self._handle_codeactions, params)
+                self.view, '/v2/getcodeactions', self._handle_codeactions, params)
         else:
             self._show_code_actions_view(edit)
 
@@ -52,18 +55,20 @@ class OmniSharpCodeActions(sublime_plugin.TextCommand):
         print('codeactions is :')
         print(self.data)
         self.quickitems = [];
+        self.quickitemids = [];
         if "CodeActions" in self.data and self.data["CodeActions"] != None:
             for i in self.data["CodeActions"]:
                 print(i)
-                self.quickitems.append(i.strip())
+                self.quickitems.append(i["Name"])
+                self.quickitemids.append(i["Identifier"])
         if len(self.quickitems) > 0:
-            self.view.window().show_quick_panel(self.quickitems, self.on_done)
+            self.view.show_popup_menu(self.quickitems, self.on_done)
         else:
             self.data = None
-            self.selectionEndLine = 0
-            self.selectionEndColumn = 0
-            self.selectionStartLine = 0
-            self.selectionStartColumn = 0
+            self.selection["End"]["Line"] = 0
+            self.selection["End"]["Column"] = 0
+            self.selection["Start"]["Line"] = 0
+            self.selection["Start"]["Column"] = 0
 
     def is_enabled(self):
         return helpers.is_csharp(self.view)
@@ -71,26 +76,25 @@ class OmniSharpCodeActions(sublime_plugin.TextCommand):
     def on_done(self, index):
         if index == -1:
             self.data = None
-            self.selectionEndLine = 0
-            self.selectionEndColumn = 0
-            self.selectionStartLine = 0
-            self.selectionStartColumn = 0
+            self.selection["End"]["Line"] = 0
+            self.selection["End"]["Column"] = 0
+            self.selection["Start"]["Line"] = 0
+            self.selection["Start"]["Column"] = 0
             return
 
         print("run index: " + str(index))
 
         params = {}
-        params['codeAction'] = index
-        params['selectionStartColumn'] = self.selectionStartColumn
-        params['selectionStartLine'] = self.selectionStartLine
-        params['selectionEndColumn'] = self.selectionEndColumn
-        params['selectionEndLine'] = self.selectionEndLine
-        omnisharp.get_response(self.view, '/runcodeaction', self._handle_runcodeaction, params)
+        params['Identifier'] = self.quickitemids[index]
+        params['Selection'] = self.selection
+        params['WantsTextChanges'] = True
+        params['WantsAllCodeActionOperations'] = True
+        omnisharp.get_response(self.view, '/v2/runcodeaction', self._handle_runcodeaction, params)
         self.data = None
-        self.selectionEndLine = 0
-        self.selectionEndColumn = 0
-        self.selectionStartLine = 0
-        self.selectionStartColumn = 0
+        self.selection["End"]["Line"] = 0
+        self.selection["End"]["Column"] = 0
+        self.selection["Start"]["Line"] = 0
+        self.selection["Start"]["Column"] = 0
         
     def _handle_runcodeaction(self, data):
         print('runcodeaction is:')
@@ -98,9 +102,20 @@ class OmniSharpCodeActions(sublime_plugin.TextCommand):
         if data is None:
             return
         
-        self.view.run_command("omni_sharp_run_code_action",{"args":{'text':data['Text']}})
+        for change in data['Changes']:
+            self.view.run_command("omni_sharp_run_code_action",{"args":{'change':change}})
 
 class OmniSharpRunCodeAction(sublime_plugin.TextCommand):
   def run(self, edit, args):
-    region = sublime.Region(0, self.view.size())
-    self.view.replace(edit, region, args['text'])
+    view = self.view.window().open_file(args['change']['FileName'])
+
+    while(view.is_loading()):
+        pass
+
+    for change in args['change']['Changes']:
+        region = sublime.Region(
+            view.text_point(change['StartLine'] - 1, change['StartColumn'] - 1),
+            view.text_point(change['EndLine'] - 1, change['EndColumn'] - 1)
+        )
+
+        view.replace(edit, region, change['NewText'])
